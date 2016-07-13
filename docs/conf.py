2f4acd7e8956e84d8969fe9242213e203c252949
Dwayne Bailey <dwayne@translate.org.za>
2016-07-13 15:50:51 +0100

    Docs: match Pootle attribution

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Translate Project'
-copyright = u'2016, Translate contributors'
+copyright = u'2016, Translate'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
